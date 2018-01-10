1435e160140d122303d759cede7973b5b90cb52b
Miniaylo.S <po4tovuj@gmail.com>
2018-01-10 13:49:13 +0200

    add clearing input, and autoupdate user-list

--- miniajlo_s-js_m-11/index.js
+++ miniajlo_s-js_m-11/index.js
@@ -1,11 +1,11 @@
 let tBody    = document.querySelector("#js-tbody"),
     htmlTpl    = document.querySelector("#table-row").textContent.trim(),
     compiled   = _.template(htmlTpl),
-    refresh    = document.querySelector('#refresh'),
     url        = "http://fecore.net.ua/rest/",
+    get        = document.querySelector('#get')
     btnAdd     = document.querySelector('#addUser'),
     btnDel     = document.querySelector('.delete'),
-    btnRefresh = document.querySelector('#refresh'),
+    btnRefresh = document.querySelector('#update'),
     updateView = users => {
         let htmlString = "";
 
@@ -32,62 +32,37 @@ const getUsers = () => {
 };
 
 const addUser = () => {
-    let user = document.querySelector('#user').value,
-        score = document.querySelector('#score').value,
-        newUrl = `${url}?action=1&name=${user}&score=${score}`;
-    document.querySelector('#score').value = "";
-    document.querySelector('#user').value = "";
-
-    fetch(newUrl, {
-            method: "post",
-            mode: "cors",
-        })
-            .then(response => {
-                if (response.ok) return response.json();
-                throw new Error("Error fetching data");
-            })
+    newUrl = `${url}?action=1&name=${document.querySelector('#user').value}&score=${document.querySelector('#score').value}`;
+    document.querySelector('.add-user').reset();
+    fetch(newUrl)
+        .then(getUsers)
                 .catch(err => {
                     console.error("Error: ", err);
                 });
 };
 
 const removeUser = () => {
-    let id      = document.querySelector('#restId').value,
-        newUrl  = `${url}?action=3&id=${id}`;
-    document.querySelector('#restId').value = "";
-    fetch(newUrl, {
-            method: 'post',
-        })
-            .then(response => {
-                if (response.ok) return response.json();
-                throw new Error("Error fetching data");
-            })
+    newUrl = `${url}?action=3&id=${document.querySelector('#restId').value}`;
+    document.querySelector('.del').reset();
+    fetch(newUrl)
+        .then(getUsers)
                 .catch(err => {
                     console.error("Error: ", err);
                 });
 };
 
 const updateUser = () => {
-    let searchId     = document.querySelector("#searchId").value,
-        refreshName  = document.querySelector('#refreshName').value,
-        refreshScore = document.querySelector('#refreshScore').value;
-        newUrl       = `${url}?action=2&id=${searchId}&name=${refreshName}&score=${refreshScore}`;
-        document.querySelector("#searchId").value = "";
-        document.querySelector('#refreshName').value = "";
-        document.querySelector('#refreshScore').value = "";
-    fetch(newUrl, {
-            method: 'post',
-        })
-        .then(response => {
-            if (response.ok) return response.json();
-            throw new Error("Error fetching data");
-        })
+    newUrl = `${url}?action=2&id=${document.querySelector("#searchId").value}&name=${document.querySelector('#refreshName').value}&score=${document.querySelector('#refreshScore').value}`;
+    document.querySelector('.update').reset();
+        
+    fetch(newUrl)
+        .then(getUsers)
         .catch(err => {
             console.error("Error: ", err);
         });
 };
 
-refresh.addEventListener('click', getUsers);
+get.addEventListener('click', getUsers);
 btnAdd.addEventListener('click', addUser);
 btnDel.addEventListener('click', removeUser);
 btnRefresh.addEventListener('click', updateUser);
